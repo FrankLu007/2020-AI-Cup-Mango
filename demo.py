bce136a6345199beb42fe37a40c16d8b8d80e8aa
Frank <shes990106@gmail.com>
2020-11-25 02:49:43 +0800

    grade competition end

--- demo.py
+++ demo.py
@@ -15,11 +15,10 @@ if __name__ == '__main__' :
 
     args = get_args()
     ModelPath = '../'
-    DataPath = '../Test/'
+    DataPath = '../MangoData/merge/test/'
 
     transform_demo = torchvision.transforms.Compose([
-        torchvision.transforms.Resize((int(args['size'] * 1.5), int(args['size'] * 1.5))),
-        torchvision.transforms.RandomCrop((args['size'], args['size'])),
+        torchvision.transforms.Resize((args['size'], args['size'])),
         torchvision.transforms.RandomRotation(180),
         torchvision.transforms.RandomHorizontalFlip(),
         torchvision.transforms.GaussianBlur(7, 10),
@@ -32,20 +31,22 @@ if __name__ == '__main__' :
     model = torch.load(ModelPath + args['load'])
     model.eval()
 
-    # print('image_id,label')
+    print('image_id,label')
 
     label = ['A', 'B', 'C']
-    index = 10726
+    index = 0
     while index < len(DataList): 
-        time = 100
+        time = args['ep']
         result = torch.zeros(len(DataList[index : index + args['bs']]), 3).cuda()
+        ImageList = [torchvision.datasets.folder.pil_loader(DataPath + file[0]) for file in DataList[index : index + args['bs']]]
         while time :
-            image = torch.stack([transform_demo(torchvision.datasets.folder.pil_loader(DataPath + file[0])) for file in DataList[index : index + args['bs']]]).reshape(-1, 3, args['size'], args['size']).cuda()
+            images = torch.stack([transform_demo(image) for image in ImageList]).reshape(-1, 3, args['size'], args['size']).cuda()
             with torch.no_grad() :
                 with torch.cuda.amp.autocast():
-                    result += model(image).detach()
-            del image
+                    result += model(images).detach()
+            del images
             time -= 1
+        del ImageList
         tmp, pred = result.max(1)
         for i, file in enumerate(DataList[index : index + args['bs']]):
             print(file[0] + ',' + label[pred[i]])
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import torch.nn
 import torchvision.models
 import torch.utils.model_zoo
 from efficientnet_pytorch import EfficientNet
-from vit_pytorch import ViT
+# from vit_pytorch import ViT
 
 class EfficientNetWithFC(torch.nn.Module):
     def __init__(self):
@@ -12,12 +12,12 @@ class EfficientNetWithFC(torch.nn.Module):
     def forward(self, x):
         return self.fc(self.ImageNet(x))
 
-class Vision_Transformer(torch.nn.Module):
-    def __init__(self):
-        super(Vision_Transformer, self).__init__()
-        self.ImageNet = ViT(image_size = 224, patch_size = 32, num_classes = 3, dim = 1280, depth = 32, heads = 16, mlp_dim = 2048, dropout = 0.1, emb_dropout = 0.1)
-    def forward(self, x):
-        return self.ImageNet(x)
+# class Vision_Transformer(torch.nn.Module):
+#     def __init__(self):
+#         super(Vision_Transformer, self).__init__()
+#         self.ImageNet = ViT(image_size = 224, patch_size = 32, num_classes = 3, dim = 1280, depth = 32, heads = 16, mlp_dim = 2048, dropout = 0.1, emb_dropout = 0.1)
+#     def forward(self, x):
+#         return self.ImageNet(x)
 
 def resnext101_32x48d_wsl(progress = True, **kwargs):
     kwargs['groups'] = 32
@@ -27,7 +27,7 @@ def resnext101_32x48d_wsl(progress = True, **kwargs):
     model.load_state_dict(state_dict)
     return model
 
-def FixResNet(torch.nn.Module):
+class FixResNet(torch.nn.Module):
     def __init__(self):
         super(FixResNet, self).__init__()
         self.ImageNet = resnext101_32x48d_wsl()
